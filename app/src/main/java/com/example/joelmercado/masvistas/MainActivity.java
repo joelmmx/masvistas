24cc9ac3de4abcf0b6acb292f75113cfc26cedf6
Joel Mercado <joelmmx@gmail.com>
2018-02-18 18:54:10 -0600

    Realizado proyecto como en el libro
    app/src/main/java/com/example/joelmercado/masvistas/MainActivity.java
    app/src/main/res/layout/activy_main.xml
    app/src/main/res/values/colors.xml

--- app/src/main/java/com/example/joelmercado/masvistas/MainActivity.java
+++ app/src/main/java/com/example/joelmercado/masvistas/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,16 +14,29 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    private EditText entrada;
+    private TextView salida;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        /*TextView textView=new TextView(this);
+        TextView textView=new TextView(this);
         textView.setText("Mi nueva actividad");
-        setContentView(textView);*/
+        setContentView(textView);
         setContentView(R.layout.activy_main);
+        entrada=(EditText)findViewById(R.id.entrada);
+        salida=(TextView)findViewById(R.id.salida);
     }
 
-    public void sePulsa(View view){
+   /* public void sePulsa(View view){
         Toast.makeText(this,"Pulsado",Toast.LENGTH_SHORT).show();
+    }*/
+
+    public void sePulsa(View view){
+        salida.setText(String.valueOf(Float.parseFloat(entrada.getText().toString())*2.0));
+    }
+
+    public void sePulsa0(View view){
+        entrada.setText(entrada.getText()+(String)view.getTag());
     }
 }
